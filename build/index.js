8d0dd07cdd85a72c397dc2a4e544ea5dea22ef44
supawish34 <supawish.kj@gmail.com>
2021-08-20 11:40:42 +0700

    Update index.js

--- build/index.js
+++ build/index.js
@@ -1,1 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get("./", (req, res) => {
+    const age = 39;
+    res.json({ message: "Please Like this" });
+});
+app.listen("3001", () => {
+    console.log("Server Running!");
+});
