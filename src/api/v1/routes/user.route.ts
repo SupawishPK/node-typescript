30d80f8301c6914071824c175e5d879965092590
supawish34 <supawish.kj@gmail.com>
2021-08-25 14:20:18 +0700

    posts

--- src/api/v1/routes/user.route.ts
+++ src/api/v1/routes/user.route.ts
@@ -1,14 +1,26 @@
 import { Express, Request, Response } from 'express'
 import {
+  createPostSchema,
+  deletePostSchema,
+  updatePostSchema,
+} from '../../../schema/post.schema'
+import {
   createUserSchema,
   createUserSessionSchema,
 } from '../../../schema/user.schema'
 import {
+  createPostHandler,
+  deletePostHandler,
+  getPostHandler,
+  updatePostHandler,
+} from '../controllers/post.controller'
+import {
   createUserSessionHandler,
   invalidateUserSessionHandler,
-  getUserSessionsHandler
+  getUserSessionsHandler,
 } from '../controllers/session.controller'
 import { createUserHandler } from '../controllers/user.controller'
+
 import { validateRequest, requiresUser } from '../middlewares'
 
 export default function (app: Express) {
@@ -18,35 +30,39 @@ export default function (app: Express) {
   app.post('/api/users', validateRequest(createUserSchema), createUserHandler)
 
   // Login
-  app.post('/api/sessions',validateRequest(createUserSessionSchema),createUserSessionHandler)
+  app.post(
+    '/api/sessions',
+    validateRequest(createUserSessionSchema),
+    createUserSessionHandler
+  )
 
-  // // Get the user's sessions
-  app.get("/api/sessions", requiresUser, getUserSessionsHandler);
+  // Get the user's sessions
+  app.get('/api/sessions', requiresUser, getUserSessionsHandler)
 
   // Logout
   app.delete('/api/sessions', requiresUser, invalidateUserSessionHandler)
 
-  // // Create a post
-  // app.post(
-  //   "/api/posts",
-  //   [requiresUser, validateRequest(createPostSchema)],
-  //   createPostHandler
-  // );
+  // Create a post
+  app.post(
+    '/api/posts',
+    [requiresUser, validateRequest(createPostSchema)],
+    createPostHandler
+  )
 
-  // // Update a post
-  // app.put(
-  //   "/api/posts/:postId",
-  //   [requiresUser, validateRequest(updatePostSchema)],
-  //   updatePostHandler
-  // );
+  // Update a post
+  app.put(
+    '/api/posts/:postId',
+    [requiresUser, validateRequest(updatePostSchema)],
+    updatePostHandler
+  )
 
-  // // Get a post
-  // app.get("/api/posts/:postId", getPostHandler);
+  // Get a post
+  app.get('/api/posts/:postId', getPostHandler)
 
-  // // Delete a post
-  // app.delete(
-  //   "/api/posts/:postId",
-  //   [requiresUser, validateRequest(deletePostSchema)],
-  //   deletePostHandler
-  // );
+  // Delete a post
+  app.delete(
+    '/api/posts/:postId',
+    [requiresUser, validateRequest(deletePostSchema)],
+    deletePostHandler
+  )
 }
